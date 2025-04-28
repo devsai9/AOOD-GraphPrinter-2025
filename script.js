2653c8eef876058c19af9a3e6dcbd863332b4da8
Dev Sai <63507117+devsai9@users.noreply.github.com>
2025-04-28 10:40:16 -0400

    [WIP] Work done via CodeHS

--- script.js
+++ script.js
@@ -24,24 +24,27 @@ const yAnnotate = document.getElementById("y-annotate-interval");
 const xLabel = document.getElementById("x-label");
 const yLabel = document.getElementById("y-label");
 
-/*
-var xMinValue = -5;
-xMin.addEventListener("change",(event) => {
-    xMinValue = document.getElementById("xMin").value;
-});
+const arrowCheckbox = document.getElementById("arrowCheckbox");
+const radianCheckbox = document.getElementById("radianCheckbox");
 
-*/
 
-adjustCanvasDimensions();
-function adjustCanvasDimensions() {
+// document.body.addEventListener("keypress", update);
+// document.body.addEventListener("click", update);
+document.querySelectorAll("input").forEach((elem) => elem.addEventListener("change", update));
+window.addEventListener("resize", update);
+
+update();
+function update() {
     const min = Math.min(rightPanel.offsetWidth, rightPanel.offsetHeight - rightPanel.children[0].offsetHeight)
     canvas.width = min;
     canvas.height = min;
     declareCanvasVars();
     drawAxes();
+    drawTicks();
+    if (arrowCheckbox.checked){
+        drawArrows();
+    }
 }
-
-window.addEventListener("resize", adjustCanvasDimensions);
 
 function declareCanvasVars() {
     lineWidth = 1;
@@ -52,12 +55,24 @@ function declareCanvasVars() {
     centerY = canvas.height / 2;
 }
 
+// function drawAxes() {
+//     ctx.fillStyle = "rgb(0, 0, 0)";
+//     // const axesLength = 2 * Math.min(xRadius, yRadius);
+//     ctx.fillRect(centerX - xRadius, centerY, 2 * xRadius, lineWidth);
+//     ctx.fillRect(centerX, centerY - yRadius, lineWidth, 2 * yRadius);
+// }
+
 function drawAxes() {
-    ctx.fillStyle = "rgb(0, 0, 0)";
-    // const axesLength = 2 * Math.min(xRadius, yRadius);
-    ctx.fillRect(centerX - xRadius, centerY, 2 * xRadius, lineWidth);
-    ctx.fillRect(centerX, centerY - yRadius, lineWidth, 2 * yRadius);
+    ctx.fill = "rbg(0, 0, 0)";
     
+    // X axis
+    ctx.fillRect(centerX - xRadius, xMax * xRadius * 2 / (xMax - xMin), 2 * xRadius, lineWidth);
+    
+    // Y axis
+    ctx.fillRect(yMax * yRadius * 2 / (yMax - yMin), centerY - yRadius, lineWidth, 2 * yRadius);
+}
+
+function drawArrows(){
     // draw arrows
     const arrowSize = canvas.width / 100;
     ctx.beginPath();
@@ -90,7 +105,9 @@ function drawAxes() {
 }
 
 function drawTicks() {
-    const tickOffset = 5;
+    ctx.fillStyle = "rgb(0, 0, 0)";
+
+    const tickOffset = 15; // because don't draw ticks too close to the end
     
     const xTickInterval = parseInt(xTicks.value); // will get from x Tick Interval
     const yTickInterval = parseInt(yTicks.value);
@@ -98,10 +115,65 @@ function drawTicks() {
     const xNumericRange = parseInt(xMax.value) - parseInt(xMin.value);
     const yNumericRange = parseInt(yMax.value) - parseInt(yMin.value);
     
-    const xCanvasFrequency = xTickInterval / xNumericRange * 2 * (xRadius - tickOffset);
-    const yCanvasFrequency = yTickInterval / yNumericRange * 2 * (yRadius - tickOffset);
+    const xCanvasFrequency = (xTickInterval / xNumericRange) * 2 * (xRadius - tickOffset);
+    const yCanvasFrequency = (yTickInterval / yNumericRange) * 2 * (yRadius - tickOffset);
     
+    let curX = parseInt(xMin.value);
     
+    let xAxisLocation = 0; // this is a y value lmao
+    if (0 <= yMin) xAxisLocation = centerY + yRadius - tickOffset; // bottom of page
+    if (yMax <= 0) xAxisLocation = centerY - yRadius + tickOffset; // top of page
+    
+    let dummyYMark = parseInt(yMax.value); // top to bottom is max to min
+    for (let i = (centerY - yRadius + tickOffset); i <= (centerY + yRadius - tickOffset); i += yCanvasFrequency) {
+        if (dummyYMark == 0) {
+            xAxisLocation = i;
+            break;
+        }
+        dummyYMark -= 1;
+    }
+    
+    for (let i = (centerX - xRadius + tickOffset); i <= (centerX + xRadius - tickOffset); i += xCanvasFrequency) {
+        // draw ticks
+        let tickLength = 20;
+        ctx.beginPath();
+        ctx.moveTo(i, xAxisLocation - tickLength * 0.5);
+        ctx.lineTo(i, xAxisLocation + tickLength * 0.5);
+        ctx.stroke();
+        // console.log("drew x tick at", i, "y-coordinate", centerY - tickLength * 0.5);
+        
+        // draw labels
+        ctx.font = "16px serif";
+        ctx.fillText(curX, i, xAxisLocation - tickLength);
+        curX += 1;
+    }
+    
+    let yAxisLocation = 0; // this is a y value lmao
+    if (0 <= xMin) yAxisLocation = centerX - xRadius + tickOffset; // left of page
+    if (xMax <= 0) yAxisLocation = centerX + xRadius - tickOffset; // right of page
+    
+    let dummyXMark = parseInt(xMin.value); // top to bottom is max to min
+    for (let i = (centerX - xRadius + tickOffset); i <= (centerX + xRadius - tickOffset); i += xCanvasFrequency) {
+        if (dummyXMark == 0) {
+            yAxisLocation = i;
+            break;
+        }
+        dummyXMark += 1;
+    }
+    let curY = parseInt(yMax.value);
+    for (let i = (centerY - yRadius + tickOffset); i <= (centerY + yRadius - tickOffset); i += yCanvasFrequency) {
+        // draw ticks
+        let tickLength = 20;
+        ctx.beginPath();
+        ctx.moveTo(yAxisLocation - tickLength * 0.5, i);
+        ctx.lineTo(yAxisLocation + tickLength * 0.5, i);
+        ctx.stroke();
+        
+        // draw labels
+        ctx.font = "16px serif";
+        ctx.fillText(curY, yAxisLocation + tickLength, i);
+        curY -= 1;
+    }
 }
 
 // ctx.fillStyle = "green";
